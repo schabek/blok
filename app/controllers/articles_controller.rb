d549cd81ab248b68bc2eef75a58d21106ae120be
nana_nana <kaban_office@wp.pl>
2020-10-30 11:19:54 +0100

    Added update and edit option

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,6 +8,10 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
     if @article.save
@@ -15,6 +19,15 @@ class ArticlesController < ApplicationController
     redirect_to @article
     else
       render 'new'
+    end
+  end
+  def update
+    @article = Article.find(params[:id])
+     if @article.update(params.require(:article).permit(:title, :description))
+       flash[:notice] = "Article was updated successfully"
+       redirect_to @article
+    else
+      render 'edit'
     end
   end
 end
